5ed4fbbe797fa9b8b3d2c4cc5c429565cb439089
Leonardo Mora López <41559395+leonardomoralopez89@users.noreply.github.com>
2023-03-02 14:20:03 +0200

    Update paginator.go

--- paginator.go
+++ paginator.go
@@ -7,35 +7,29 @@ import (
 )
 
 type Pagination struct {
-	Limit  int    `json:"limit"`
-	Page   int    `json:"page"`
-	Sort   string `json:"sort"`
+	Limit  int         `json:"limit,omitempty"`
+	Page   int         `json:"page,omitempty"`
+	Sort   string      `json:"sort,omitempty"`
+	Total  int64       `json:"total"`
+	Pages  int         `json:"pages"`
+	Rows   interface{} `json:"rows"`
 	Offset int
 }
 
-func GenerateFromRequest(c *gin.Context) Pagination {
+func New(c *gin.Context) Pagination {
 	p := Pagination{}
-	query := c.Request.URL.Query()
-	for k, v := range query {
-		switch k {
-		case "limit":
-			p.Limit, _ = strconv.Atoi(v[0])
-			if p.Limit <= 0 {
-				p.Limit = 10
-			}
-		case "page":
-			p.Page, _ = strconv.Atoi(v[0])
-			if p.Page <= 0 {
-				p.Page = 1
-			}
-		case "sort":
-			sort := make([]string, 0)
-			for _, s := range v {
-				sort = append(sort, strings.Replace(s, ",", " ", 1))
-			}
-			p.Sort = strings.Join(sort, ",")
-		}
-	}
+
+	p.Page, _ = strconv.Atoi(c.DefaultQuery("page", "1"))
+	p.Limit, _ = strconv.Atoi(c.DefaultQuery("limit", "10"))
 	p.Offset = (p.Page - 1) * p.Limit
+	sort := c.QueryArray("sort")
+
+	if len(sort) > 0 {
+		srt := make([]string, 0)
+		for _, s := range sort {
+			srt = append(srt, strings.Replace(s, ",", " ", 1))
+		}
+		p.Sort = strings.Join(srt, ",")
+	}
 	return p
 }
